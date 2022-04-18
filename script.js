b12c0bcc95d6d887a2d762f343268cf1d31b7366
braddefauw <badefauw@gmail.com>
2022-04-18 10:46:07 -0500

    gameboard module + player factory functions established, + icon added to html

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("go heels go america")
+// gameboard module
+const gameBoard = (() => {
+    const board = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+    return { board };
+})();
+
+// console.log(gameBoard.board);
+
+// player factory function
+const Player = name => {
+    const getName = () => console.log(name);
+    return { getName };
+}
+
+const brad = Player('brad');
+// brad.getName();

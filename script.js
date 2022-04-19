53053d4f2e88d7a2b003d9fd7a68c5bbd79b1b07
braddefauw <badefauw@gmail.com>
2022-04-19 12:00:09 -0500

    update message based on winning team + disallow further moves upon game completion

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const brad = Player('brad');
 let turn = 1;
 let index = 0;
 
+let footer = document.querySelector('footer');
+
 gameBoard.board.forEach((element) => {
     let block = document.createElement("div");
     block.classList.add("block");
@@ -23,17 +25,19 @@ gameBoard.board.forEach((element) => {
     index++;
     block.innerText = element;
     block.addEventListener('click', () => {
-        if(block.innerText === ""){
-            if(turn % 2 !== 0){
-                element = "X";
-            }else{
-                element = "O";
+        if(footer.innerText === "In Progress..."){
+            if(block.innerText === ""){
+                if(turn % 2 !== 0){
+                    element = "X";
+                }else{
+                    element = "O";
+                }
+                block.innerText = element;
+                gameBoard.board[block.dataset.indexNumber] = element;
+                check();
+                turn++;
             }
-            block.innerText = element;
-            gameBoard.board[block.dataset.indexNumber] = element;
-            check();
-            turn++;
-        }
+        } 
     })
     let boardDiv = document.querySelector('#board');
     boardDiv.appendChild(block);
@@ -48,7 +52,7 @@ const check = () => {
     (gameBoard.board[2] === "X" && gameBoard.board[5] === "X" && gameBoard.board[8] === "X") ||
     (gameBoard.board[0] === "X" && gameBoard.board[4] === "X" && gameBoard.board[8] === "X") ||
     (gameBoard.board[2] === "X" && gameBoard.board[4] === "X" && gameBoard.board[6] === "X")){
-        console.log("x wins");
+        footer.innerText = "X WINS!"
     }else if((gameBoard.board[0] === "O" && gameBoard.board[1] === "O" && gameBoard.board[2] === "O") ||
     (gameBoard.board[3] === "O" && gameBoard.board[4] === "O" && gameBoard.board[5] === "O") ||
     (gameBoard.board[6] === "O" && gameBoard.board[7] === "O" && gameBoard.board[8] === "O") ||
@@ -57,9 +61,9 @@ const check = () => {
     (gameBoard.board[2] === "O" && gameBoard.board[5] === "O" && gameBoard.board[8] === "O") ||
     (gameBoard.board[0] === "O" && gameBoard.board[4] === "O" && gameBoard.board[8] === "O") ||
     (gameBoard.board[2] === "O" && gameBoard.board[4] === "O" && gameBoard.board[6] === "O")){
-        console.log("o wins");
+        footer.innerText = "O WINS!"
     }else if(gameBoard.board[0] && gameBoard.board[1] && gameBoard.board[2] && gameBoard.board[3] &&
         gameBoard.board[4] && gameBoard.board[5] && gameBoard.board[6] && gameBoard.board[7] && gameBoard.board[8]){
-        console.log("draw")
+            footer.innerText = "DRAW"
     }
 }

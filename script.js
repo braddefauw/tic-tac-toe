09bc88295703e84419f59925b60e8656e88f27e8
braddefauw <badefauw@gmail.com>
2022-04-19 12:13:23 -0500

    added hover capabilities and updated color of message depending on winning team

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const brad = Player('brad');
 let turn = 1;
 let index = 0;
 
-let footer = document.querySelector('footer');
+let message = document.querySelector('.message');
 
 gameBoard.board.forEach((element) => {
     let block = document.createElement("div");
@@ -25,7 +25,7 @@ gameBoard.board.forEach((element) => {
     index++;
     block.innerText = element;
     block.addEventListener('click', () => {
-        if(footer.innerText === "In Progress..."){
+        if(message.innerText === "In Progress..."){
             if(block.innerText === ""){
                 if(turn % 2 !== 0){
                     element = "X";
@@ -52,7 +52,8 @@ const check = () => {
     (gameBoard.board[2] === "X" && gameBoard.board[5] === "X" && gameBoard.board[8] === "X") ||
     (gameBoard.board[0] === "X" && gameBoard.board[4] === "X" && gameBoard.board[8] === "X") ||
     (gameBoard.board[2] === "X" && gameBoard.board[4] === "X" && gameBoard.board[6] === "X")){
-        footer.innerText = "X WINS!"
+        message.innerText = "X WINS!";
+        message.style.color = "rgb(255, 230, 0)";
     }else if((gameBoard.board[0] === "O" && gameBoard.board[1] === "O" && gameBoard.board[2] === "O") ||
     (gameBoard.board[3] === "O" && gameBoard.board[4] === "O" && gameBoard.board[5] === "O") ||
     (gameBoard.board[6] === "O" && gameBoard.board[7] === "O" && gameBoard.board[8] === "O") ||
@@ -61,9 +62,10 @@ const check = () => {
     (gameBoard.board[2] === "O" && gameBoard.board[5] === "O" && gameBoard.board[8] === "O") ||
     (gameBoard.board[0] === "O" && gameBoard.board[4] === "O" && gameBoard.board[8] === "O") ||
     (gameBoard.board[2] === "O" && gameBoard.board[4] === "O" && gameBoard.board[6] === "O")){
-        footer.innerText = "O WINS!"
+        message.innerText = "O WINS!";
+        message.style.color = "rgb(8, 201, 201)";
     }else if(gameBoard.board[0] && gameBoard.board[1] && gameBoard.board[2] && gameBoard.board[3] &&
         gameBoard.board[4] && gameBoard.board[5] && gameBoard.board[6] && gameBoard.board[7] && gameBoard.board[8]){
-            footer.innerText = "DRAW"
+            message.innerText = "DRAW"
     }
 }
